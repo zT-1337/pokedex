c2484dba34a0b5d564efdb2855e3e51b5d1b03c7
Thomas Zerr <thomas.zerr@gmail.com>
2023-03-17 16:44:21 +0100

    -added: gradient border color if pokemon has two types

--- app/src/lib/pokedex/components/pokemon-image.svelte
+++ app/src/lib/pokedex/components/pokemon-image.svelte
@@ -3,8 +3,22 @@
 	import { typeToColor } from "../utils/pokemon-type";
 
   export let pokemon: Pokemon;
+
+  function getBorderColor(pokemon: Pokemon) {
+    if(pokemon.secondaryType === undefined) {
+      return `border-color: ${typeToColor(pokemon.primaryType)}`;
+    }
+
+    const primaryTypeColor = typeToColor(pokemon.primaryType);
+    const secondaryTypeColor = typeToColor(pokemon.secondaryType);
+
+    return `border-image: linear-gradient(135deg, ${primaryTypeColor} 0%, ${primaryTypeColor} 50%, ${secondaryTypeColor} 50%, ${secondaryTypeColor} 100%) 1`;
+  }
 </script>
 
-<div class={`bg-base-100 rounded-2xl border-4`} style={`border-color: ${typeToColor(pokemon.primaryType)}`}>
+<div 
+  class={`bg-base-100 border-4 border-solid`} 
+  style={getBorderColor(pokemon)}
+>
   <img width="475" height="475" src={pokemon.imageUrl} alt={`Picture of ${pokemon.name}`}/>
 </div>
